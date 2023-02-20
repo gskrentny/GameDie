7778bd3c6f8741cfd4bed4e56ce7cf690d6611f3
gskrentny <47639988+gskrentny@users.noreply.github.com>
2023-02-20 11:58:09 -0800

    Add files via upload

--- GameDie.h
+++ GameDie.h
@@ -11,9 +11,10 @@ class GameDie
    GameDie(unsigned int);
    int roll();
    vector <int> get_distribution();
+   vector<double> get_percentages();
 
   private:
-   vector <int> roll_counter;
+   vector <int> counter;
    const static int FACES = 6;
 };
 
